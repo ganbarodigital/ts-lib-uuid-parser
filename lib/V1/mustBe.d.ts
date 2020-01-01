498cc9a6622b94010e2ba2f8d6e8259b23c8e861
Stuart Herbert <stuart@stuartherbert.com>
2020-01-01 10:30:32 +0000

    Fix: more fixes for NPM publishing

---
+++ lib/V1/mustBe.d.ts
@@ -0,0 +1,6 @@
+import { Uuid } from './types/Uuid';
+/**
+ * throws an error if the given string is not a well-formatted UUID
+ */
+export declare function mustBe(input: Uuid | string): void;
+//# sourceMappingURL=mustBe.d.ts.map
